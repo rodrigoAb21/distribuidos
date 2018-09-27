dc628d227533c39b155766b74c478b903aed60b4
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-09-27 13:54:32 -0400

    casi completo
    jeje

--- laravel/app/Http/Controllers/Web/InformeController.php
+++ laravel/app/Http/Controllers/Web/InformeController.php
@@ -14,7 +14,7 @@ class ReporteController extends Controller
      */
     public function index()
     {
-        //
+        return view('reportes.index');
     }
 
     /**
@@ -46,7 +46,7 @@ class ReporteController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('reportes.show');
     }
 
     /**
