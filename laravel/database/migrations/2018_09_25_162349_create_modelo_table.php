d51ef82559717dea472683a82551a43582a43db8
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-09-30 17:15:06 -0400

    pregunta
    ya da crear pregunta

--- laravel/database/migrations/2018_09_25_162349_create_modelo_table.php
+++ laravel/database/migrations/2018_09_25_162349_create_modelo_table.php
@@ -13,7 +13,7 @@ class CreateModeloEncuestaTable extends Migration
      */
     public function up()
     {
-        Schema::create('modelo_encuesta', function (Blueprint $table) {
+        Schema::create('modelo', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre');
             $table->string('estado');
