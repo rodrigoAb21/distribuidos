36fa5b19db615da24aaafa50dace0597a9f04953
nath <nath.1475369@gmail.com>
2018-09-30 19:25:43 -0400

    tipoP ok
    oculta o muestra según tipo de pregunta

--- laravel/resources/views/modelos/edit.blade.php
+++ laravel/resources/views/modelos/edit.blade.php
@@ -86,10 +86,11 @@
     @push('scripts')
         <script>
             var cont = 0;
+            $('#tipoP').change(evaluar);
         function agregar() {
             cont++;
             var texto =$('#texto').val();
-            var fila='<tr id="fila'+cont+'"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar('+cont+');"><i        class="fa fa-trash" aria-hidden="true"></i></button></td><td><input type="hidden" name="textoT[]" value="'+texto+'"/>            '+texto+'</td></tr>';
+            var fila='<tr id="fila'+cont+'"><td><button type="button" class="btn btn-danger btn-sm" onclick="eliminar('+cont+');"><i class="fa fa-trash" aria-hidden="true"></i></button></td><td><input type="hidden" name="textoT[]" value="'+texto+'"/>'+texto+'</td></tr>';
             $("#tabla").append(fila);
             $('#texto').val("");
         }
@@ -97,6 +98,26 @@
         function eliminar(index) {
             $("#fila" + index).remove();
             cont--;
+        }
+
+        function esconder() {
+            vaciarT();
+            $('#contOpciones').hide();
+        }
+
+        function vaciarT() {
+            while (cont>0){
+                eliminar(cont);
+            }
+
+        }
+        
+        function evaluar() {
+            if($('#tipoP option:selected').text() == 'Entrada de texto'){
+                esconder();
+            }else {
+                $('#contOpciones').show();
+            }
         }
         </script>
     @endpush
--- laravel/resources/views/modelos/modales/pmodal.blade.php
+++ laravel/resources/views/modelos/modales/pmodal.blade.php
@@ -21,7 +21,7 @@
                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                             <div class="form-group">
                                 <label>Tipo</label>
-                                <select name="tipop" class="form-control">
+                                <select id="tipoP" name="tipop" class="form-control">
                                     <option value="Seleccion Unica">Seleccion Unica</option>
                                     <option value="Seleccion Multiple">Seleccion Multiple</option>
                                     <option value="Entrada de texto">Entrada de texto</option>
@@ -55,34 +55,37 @@
                         </div>
                     </div>
                     <hr>
-                    <div class="row">
-                        <div class="container">
-                            <h4>Opciones</h4>
+                    <div id="contOpciones" >
+                        <div class="row" >
+                            <div class="container">
+                                <h4>Opciones</h4>
+                            </div>
+
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div class="form-group">
+                                    <label>Texto</label>
+                                    <input id="texto" name="texto" type="text" class="form-control" >
+                                </div>
+                                <div class="pull-right">
+                                    <button id="btnAgregar" class="btn btn-default" type="button" onclick="agregar()">Agregar</button>
+                                </div>
+                            </div>
+
                         </div>
 
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <div class="form-group">
-                                <label>Texto</label>
-                                <input id="texto" name="texto" type="text" class="form-control" >
-                            </div>
-                            <div class="pull-right">
-                                <button id="btnAgregar" class="btn btn-default" type="button" onclick="agregar()">Agregar</button>
-                            </div>
+                        <div class="table-responsive-sm">
+                            <table id="tabla" class="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th class="w-25">Opciones</th>
+                                    <th>Nombre</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
                         </div>
-
-                    </div>
-
-                    <div class="table-responsive-sm">
-                        <table id="tabla" class="table table-bordered table-hover">
-                            <thead>
-                            <tr>
-                                <th class="w-25">Opciones</th>
-                                <th>Nombre</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </table>
                     </div>
 
                 </div>
