80befa87a8a829af636f40076185f6e413b542a2
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-10-05 14:42:35 -0400

    Mejorando API
    Adios forEachs

--- laravel/app/Http/Controllers/Api/ModeloController.php
+++ laravel/app/Http/Controllers/Api/ModeloController.php
@@ -19,48 +19,8 @@ class ModeloController extends Controller
 
 
     public function obtener($id){
-        $modelo = Modelo::findOrFail($id);
-        $preguntas = $modelo->preguntas;
-        $modelo->preguntas = $preguntas;
-
-        $contador = 0;
-        foreach ($preguntas as $pregunta){
-            $cerradas = Pregunta::findOrFail($pregunta->id)->cerradas;
-            $modelo->preguntas[$contador]->cerradas = $cerradas;
-            $cont2 = 0;
-            foreach ($cerradas as $cerrada){
-                $opciones = Cerrada::findOrFail($cerrada->id)->opciones;
-                $modelo->preguntas[$contador]->cerradas[$cont2]->opciones = $opciones;
-                $cont2++;
-            }
-
-
-            $campos = Pregunta::findOrFail($pregunta->id)->campos;
-            $modelo->preguntas[$contador]->campos = $campos;
-            $cont3 = 0;
-            foreach ($campos as $campo){
-                $dominio = Campo::findOrFail($campo->id)->dominio;
-                $modelo->preguntas[$contador]->campos[$cont3]->dominio = $dominio;
-                $cont3++;
-            }
-
-            $contador++;
-        }
-
-/*
-        $modelo = Modelo::findOrFail($id);
-        $preguntas = $modelo->preguntas;
-
-        $contador = 0;
-        foreach ($preguntas as $pregunta){
-            $opciones = Pregunta::findOrFail($pregunta->id)->opciones;
-            $modelo->preguntas[$contador]->opciones = $opciones;
-            $contador++;
-        }
-
-*/
-
-        return response()->json(['data' => $modelo], 200, [], JSON_NUMERIC_CHECK);
+        $modelos = Modelo::with('preguntas', 'preguntas.cerradas', 'preguntas.campos','preguntas.campos.dominio','preguntas.cerradas.opciones')->get();
+        return response()->json(['data' => $modelos], 200, [], JSON_NUMERIC_CHECK);
 
 
     }
