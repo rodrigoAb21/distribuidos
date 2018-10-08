692f4bc239a27bd559ca65041f0c7bdbf471bf46
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-10-08 12:27:39 -0400

    Listando modelos
    :)

--- android/app/src/main/java/com/distribuidos/uagrm/android/ModeloActivity.java
+++ android/app/src/main/java/com/distribuidos/uagrm/android/ModeloActivity.java
@@ -4,37 +4,41 @@ package com.distribuidos.uagrm.android;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import com.distribuidos.uagrm.android.adapters.ModeloAdapter;
+import com.distribuidos.uagrm.android.entities.Modelo;
 import com.distribuidos.uagrm.android.entities.ModeloResponse;
 import com.distribuidos.uagrm.android.network.ApiService;
 import com.distribuidos.uagrm.android.network.RetrofitBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModeloActivity extends AppCompatActivity {
 
     private static final String TAG = "ModeloActivity";
 
-    TextView title;
-    Button btn_getModelo;
+    RecyclerView recyclerView;
 
     ApiService service;
     TokenManager tokenManager;
     Call<ModeloResponse> call;
+
+    List<Modelo> listaModelos;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modelo);
-
-        cargar();
 
         tokenManager = TokenManager.getInstance(getSharedPreferences("prefs", MODE_PRIVATE));
 
@@ -45,37 +49,47 @@ public class ModeloActivity extends AppCompatActivity {
 
         service = RetrofitBuilder.createServiceWithAuth(ApiService.class, tokenManager);
 
-        btn_getModelo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                call = service.modelos();
-                call.enqueue(new Callback<ModeloResponse>() {
-                    @Override
-                    public void onResponse(Call<ModeloResponse> call, Response<ModeloResponse> response) {
-                        Log.w(TAG, "onResponse: " + response );
+        getModelos();
 
-                        if(response.isSuccessful()){
-                            title.setText(response.body().getData().get(0).getNombre());
-                        }else {
-                            tokenManager.deleteToken();
-                            startActivity(new Intent(ModeloActivity.this, LoginActivity.class));
-                            finish();
 
-                        }
-                    }
 
-                    @Override
-                    public void onFailure(Call<ModeloResponse> call, Throwable t) {
-                        Log.w(TAG, "onFailure: " + t.getMessage() );
-                    }
-                });
-            }
-        });
     }
 
-    private void cargar(){
-        title = (TextView) findViewById(R.id.post_title);
-        btn_getModelo = (Button) findViewById(R.id.btn_getModelo);
+    private void cargarComponentes(){
+        ModeloAdapter adapter = new ModeloAdapter(listaModelos);
+        recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
+    private void getModelos(){
+        listaModelos = new ArrayList<>();
+        call = service.modelos();
+        call.enqueue(new Callback<ModeloResponse>() {
+            @Override
+            public void onResponse(Call<ModeloResponse> call, Response<ModeloResponse> response) {
+                Log.w(TAG, "onResponse: " + response );
+
+                if(response.isSuccessful()){
+                    listaModelos = response.body().getData();
+                    cargarComponentes();
+
+
+                }else {
+                    tokenManager.deleteToken();
+                    startActivity(new Intent(ModeloActivity.this, LoginActivity.class));
+                    finish();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ModeloResponse> call, Throwable t) {
+                Log.w(TAG, "onFailure: " + t.getMessage() );
+            }
+        });
     }
 
 
---
+++ android/app/src/main/java/com/distribuidos/uagrm/android/adapters/ModeloAdapter.java
@@ -0,0 +1,54 @@
+package com.distribuidos.uagrm.android.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.distribuidos.uagrm.android.R;
+import com.distribuidos.uagrm.android.entities.Modelo;
+
+import java.util.List;
+
+public class ModeloAdapter extends RecyclerView.Adapter<ModeloAdapter.ViewHolderModelos>{
+
+    List<Modelo> listaModelos;
+
+    public ModeloAdapter(List<Modelo> listaModelos) {
+        this.listaModelos = listaModelos;
+    }
+
+    @Override
+    public ModeloAdapter.ViewHolderModelos onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_recycle, null, false);
+        return new ViewHolderModelos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ModeloAdapter.ViewHolderModelos holder, int position) {
+        holder.asignarDatos(listaModelos.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaModelos.size();
+    }
+
+    public class ViewHolderModelos extends RecyclerView.ViewHolder {
+
+        TextView nombre, descripcion;
+
+        public ViewHolderModelos(View itemView) {
+            super(itemView);
+            nombre = (TextView) itemView.findViewById(R.id.nombre_item);
+            descripcion = (TextView) itemView.findViewById(R.id.descripcion_item);
+        }
+
+        public void asignarDatos(Modelo modelo){
+            this.nombre.setText(modelo.getNombre());
+            this.descripcion.setText(modelo.getDescripcion());
+        }
+    }
+}
