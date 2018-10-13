5df089055e99380cef4a7d5858896bf9a5dfe8a4
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-10-13 03:48:23 -0400

    ordenando android
    moviendo clases a paquetes

---
+++ android/app/src/main/java/com/distribuidos/uagrm/android/activities/LoginActivity.java
@@ -0,0 +1,171 @@
+package com.distribuidos.uagrm.android.activities;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.transition.TransitionManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import com.distribuidos.uagrm.android.R;
+import com.distribuidos.uagrm.android.helpers.TokenManager;
+import com.distribuidos.uagrm.android.helpers.Utils;
+import com.distribuidos.uagrm.android.entities.AccessToken;
+import com.distribuidos.uagrm.android.entities.ApiError;
+import com.distribuidos.uagrm.android.network.ApiService;
+import com.distribuidos.uagrm.android.network.RetrofitBuilder;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    TextInputLayout tilEmail;
+    TextInputLayout tilPassword;
+    RelativeLayout container;
+    LinearLayout formContainer;
+    ProgressBar loader;
+    Button login;
+
+    ApiService service;
+    TokenManager tokenManager;
+    AwesomeValidation validator;
+    Call<AccessToken> call;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        cargarElementos();
+
+        service = RetrofitBuilder.createService(ApiService.class);
+        tokenManager = TokenManager.getInstance(getSharedPreferences("prefs", MODE_PRIVATE));
+        validator = new AwesomeValidation(ValidationStyle.TEXT_INPUT_LAYOUT);
+
+        setupRules();
+
+        if(tokenManager.getToken().getAccessToken() != null){
+            startActivity(new Intent(LoginActivity.this, ModeloActivity.class));
+            finish();
+        }
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = tilEmail.getEditText().getText().toString();
+                String password = tilPassword.getEditText().getText().toString();
+
+                tilEmail.setError(null);
+                tilPassword.setError(null);
+
+                validator.clear();
+
+                if (validator.validate()) {
+                    showLoading();
+                    call = service.login(email, password);
+                    call.enqueue(new Callback<AccessToken>() {
+                        @Override
+                        public void onResponse(Call<AccessToken> call, Response<AccessToken> response) {
+
+                            Log.w(TAG, "onResponse: " + response);
+
+                            if (response.isSuccessful()) {
+                                tokenManager.saveToken(response.body());
+                                startActivity(new Intent(LoginActivity.this, ModeloActivity.class));
+                                finish();
+                            } else {
+                                if (response.code() == 422) {
+                                    handleErrors(response.errorBody());
+                                }
+                                if (response.code() == 401) {
+                                    ApiError apiError = Utils.converErrors(response.errorBody());
+                                    Toast.makeText(LoginActivity.this, apiError.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                                showForm();
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<AccessToken> call, Throwable t) {
+                            Log.w(TAG, "onFailure: " + t.getMessage());
+                            showForm();
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+
+    private void cargarElementos(){
+        tilEmail = (TextInputLayout) findViewById(R.id.til_email);
+        tilPassword = (TextInputLayout) findViewById(R.id.til_password);
+        container = (RelativeLayout) findViewById(R.id.container);
+        formContainer = (LinearLayout) findViewById(R.id.form_container);
+        loader = (ProgressBar) findViewById(R.id.loader);
+        login = (Button) findViewById(R.id.btn_login);
+    }
+
+    private void showLoading(){
+        TransitionManager.beginDelayedTransition(container);
+        formContainer.setVisibility(View.GONE);
+        loader.setVisibility(View.VISIBLE);
+    }
+
+    private void showForm(){
+        TransitionManager.beginDelayedTransition(container);
+        formContainer.setVisibility(View.VISIBLE);
+        loader.setVisibility(View.GONE);
+    }
+
+    private void handleErrors(ResponseBody response) {
+
+        ApiError apiError = Utils.converErrors(response);
+
+        for (Map.Entry<String, List<String>> error : apiError.getErrors().entrySet()) {
+            if (error.getKey().equals("username")) {
+                tilEmail.setError(error.getValue().get(0));
+            }
+            if (error.getKey().equals("password")) {
+                tilPassword.setError(error.getValue().get(0));
+            }
+        }
+
+    }
+
+    public void setupRules() {
+        validator.addValidation(this, R.id.til_email, Patterns.EMAIL_ADDRESS, R.string.err_email);
+        validator.addValidation(this, R.id.til_password, RegexTemplate.NOT_EMPTY, R.string.err_password);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (call != null) {
+            call.cancel();
+            call = null;
+        }
+    }
+}
---
+++ android/app/src/main/java/com/distribuidos/uagrm/android/helpers/Utils.java
@@ -0,0 +1,25 @@
+package com.distribuidos.uagrm.android.helpers;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+import com.distribuidos.uagrm.android.entities.ApiError;
+import com.distribuidos.uagrm.android.network.RetrofitBuilder;
+
+public class Utils {
+
+    public static ApiError converErrors(ResponseBody response){
+        Converter<ResponseBody, ApiError> converter = RetrofitBuilder.getRetrofit().responseBodyConverter(ApiError.class, new Annotation[0]);
+
+        ApiError apiError = null;
+
+        try {
+            apiError = converter.convert(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return apiError;
+    }
+}
