dbd1ebef8432470161570150327c67229e91f6dd
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-10-16 13:25:41 -0400

    Ultimos cambios escritorio
    Se mejoro un poquito la vista del formulario

--- android/app/src/main/java/com/distribuidos/uagrm/android/activities/FormularioActivity.java
+++ android/app/src/main/java/com/distribuidos/uagrm/android/activities/FormularioActivity.java
@@ -1,25 +1,14 @@
 package com.distribuidos.uagrm.android.activities;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
 import com.distribuidos.uagrm.android.R;
-import com.distribuidos.uagrm.android.entities.Campo;
-import com.distribuidos.uagrm.android.entities.Cerrada;
 import com.distribuidos.uagrm.android.entities.Modelo;
-import com.distribuidos.uagrm.android.entities.Opcion;
-import com.distribuidos.uagrm.android.entities.Pregunta;
+import com.distribuidos.uagrm.android.helpers.GeneradorEncuesta;
 import com.distribuidos.uagrm.android.helpers.TokenManager;
 import com.distribuidos.uagrm.android.network.ApiService;
 import com.distribuidos.uagrm.android.network.RetrofitBuilder;
@@ -36,6 +25,7 @@ public class FormularioActivity extends AppCompatActivity {
     TokenManager tokenManager;
     Call<ModeloResponse> call;
     String modelo_id;
+    GeneradorEncuesta generador;
     private static final String TAG = "FormularioActivity";
 
 
@@ -45,7 +35,6 @@ public class FormularioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
 
         tokenManager = TokenManager.getInstance(getSharedPreferences("prefs", MODE_PRIVATE));
-
         if(tokenManager.getToken() == null){
             startActivity(new Intent(FormularioActivity.this, LoginActivity.class));
             finish();
@@ -59,9 +48,7 @@ public class FormularioActivity extends AppCompatActivity {
         }
 
         service = RetrofitBuilder.createServiceWithAuth(ApiService.class, tokenManager);
-
         getModelo();
-
     }
 
 
@@ -72,11 +59,8 @@ public class FormularioActivity extends AppCompatActivity {
             public void onResponse(Call<ModeloResponse> call, Response<ModeloResponse> response) {
                 Log.w(TAG, "onResponse: " + response );
                 if (response.isSuccessful()){
-                    Log.w(TAG, "Soy un mensaje de todo ok" );
                     Modelo modelo = response.body().getData();
-                    Log.w(TAG, "Asigne el modelo parseado a la variable" );
                     generarVista(modelo);
-                    Log.w(TAG, "Supuestamente me mostre!" );
                 }
             }
 
@@ -90,79 +74,8 @@ public class FormularioActivity extends AppCompatActivity {
 
     private void generarVista(Modelo modelo){
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout);
-
-        for(Pregunta pregunta : modelo.getPreguntas()){
-
-            //Escribimos el enunciado de la pregunta en negritas
-            TextView textView = new TextView(getApplicationContext());
-            textView.setId(pregunta.getId());
-            textView.setText(pregunta.getEnunciado());
-            textView.setTypeface(null, Typeface.BOLD);
-            linearLayout.addView(textView);
-
-            for (Cerrada cerrada : pregunta.getCerradas()){
-
-                // Verificamos el tipo de seleccion
-                if (cerrada.getTipoSeleccion().equals("Multiple")){
-
-                    //Creamos el checkbox
-                    for (Opcion opcion : cerrada.getOpciones()){
-
-                        CheckBox checkBox = new CheckBox(getApplicationContext());
-                        checkBox.setId(opcion.getId());
-                        checkBox.setText(opcion.getTexto());
-                        linearLayout.addView(checkBox);
-
-                    }
-
-                }else{
-
-                    //Creamos el radioGroup
-                    RadioGroup radioGroup = new RadioGroup(getApplicationContext());
-                    radioGroup.setId(cerrada.getId());
-                    radioGroup.setOrientation(RadioGroup.VERTICAL);
-
-                    //Creamos los radioButtons
-                    for (Opcion opcion : cerrada.getOpciones()){
-
-                        RadioButton radioButton = new RadioButton(getApplicationContext());
-                        radioButton.setId(opcion.getId());
-                        radioButton.setText(opcion.getTexto());
-                        radioGroup.addView(radioButton);
-
-                    }
-
-                    linearLayout.addView(radioGroup);
-
-                }
-
-            }
-            for (Campo campo : pregunta.getCampos()){
-                // Agregando la etiqueta del campo
-                textView = new TextView(getApplicationContext());
-                textView.setId(campo.getId());
-                textView.setText(campo.getEtiqueta());
-                linearLayout.addView(textView);
-
-                //Agregando el editText que sera el input
-                EditText editText = new EditText(getApplicationContext());
-                editText.setId(campo.getId());
-                linearLayout.addView(editText);
-
-            }
-
-            ImageView divider = new ImageView(getApplicationContext());
-            LinearLayout.LayoutParams lp =
-                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            lp.setMargins(1, 50, 1, 50);
-            divider.setLayoutParams(lp);
-            divider.setBackgroundColor(Color.WHITE);
-            linearLayout.addView(divider);
-
-
-
-        }
-
+        generador = new GeneradorEncuesta(linearLayout, getApplicationContext());
+        generador.generarVista(modelo);
     }
 
 
---
+++ android/app/src/main/java/com/distribuidos/uagrm/android/helpers/GeneradorEncuesta.java
@@ -0,0 +1,145 @@
+package com.distribuidos.uagrm.android.helpers;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.InputType;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.distribuidos.uagrm.android.entities.Campo;
+import com.distribuidos.uagrm.android.entities.Cerrada;
+import com.distribuidos.uagrm.android.entities.Modelo;
+import com.distribuidos.uagrm.android.entities.Opcion;
+import com.distribuidos.uagrm.android.entities.Pregunta;
+
+import java.util.List;
+
+public class GeneradorEncuesta {
+    LinearLayout linearLayout; 
+    Context context;
+
+    public GeneradorEncuesta(LinearLayout linearLayout, Context context) {
+        this.linearLayout = linearLayout;
+        this.context = context;
+    }
+
+    public void generarVista(Modelo modelo){
+
+        for(Pregunta pregunta : modelo.getPreguntas()){
+            //Escribimos el enunciado de la pregunta en negritas
+            generarEnunciado(pregunta);
+            for (Cerrada cerrada : pregunta.getCerradas()){
+                // Verificamos el tipo de seleccion
+                if (cerrada.getTipoSeleccion().equals("Multiple")){
+                    //Creamos el checkbox
+                    generarOpcionesMultiples(cerrada.getOpciones());
+                }else{
+
+                    //Creamos el radioGroup
+                    generarOpcionesUnicas(cerrada.getId(), cerrada.getOpciones());
+                }
+            }
+            generarCampos(pregunta.getCampos());
+            agregarDivision();
+        }
+
+    }
+
+    private void generarOpcionesUnicas(int id, List<Opcion> opciones) {
+
+        RadioGroup radioGroup = new RadioGroup(context);
+        radioGroup.setId(id);
+        radioGroup.setOrientation(RadioGroup.VERTICAL);
+
+        //Creamos los radioButtons
+        for (Opcion opcion : opciones){
+
+            RadioButton radioButton = new RadioButton(context);
+            radioButton.setId(opcion.getId());
+            radioButton.setText(opcion.getTexto());
+            radioGroup.addView(radioButton);
+
+        }
+
+        linearLayout.addView(radioGroup);
+
+    }
+
+    private void generarOpcionesMultiples(List<Opcion> opciones) {
+
+        for (Opcion opcion : opciones){
+            CheckBox checkBox = new CheckBox(context);
+            checkBox.setId(opcion.getId());
+            checkBox.setText(opcion.getTexto());
+            linearLayout.addView(checkBox);
+        }
+
+    }
+
+    private void agregarDivision() {
+
+        ImageView divider = new ImageView(context);
+        LinearLayout.LayoutParams lp =
+                new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(1, 50, 1, 50);
+        divider.setLayoutParams(lp);
+        divider.setBackgroundColor(Color.WHITE);
+        linearLayout.addView(divider);
+
+    }
+
+
+    private void generarEnunciado(Pregunta pregunta){
+
+        TextView textView = new TextView(context);
+        textView.setId(pregunta.getId());
+        textView.setText(pregunta.getEnunciado());
+        textView.setTypeface(null, Typeface.BOLD);
+        textView.setTextColor(Color.BLACK);
+        linearLayout.addView(textView);
+
+    }
+
+    private void generarCampos(List<Campo> campos){
+
+        for (Campo campo : campos){
+            // Agregando la etiqueta del campo
+            TextView textView = new TextView(context);
+            textView.setId(campo.getId());
+            textView.setText(campo.getEtiqueta());
+            linearLayout.addView(textView);
+
+            //Agregando el editText que sera el input
+            EditText editText = new EditText(context);
+            editText.setId(campo.getId());
+
+            switch (campo.getDominio().getTipoDato()){
+                case "Texto":
+                    editText.setInputType(InputType.TYPE_CLASS_TEXT);
+                    break;
+                case "Entero":
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_SIGNED);
+                    break;
+                case "Decimal":
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER|InputType.TYPE_NUMBER_FLAG_DECIMAL|InputType.TYPE_NUMBER_FLAG_SIGNED);
+                    break;
+                case "Fecha":
+                    editText.setHint("dd/MM/YYYY");
+                    editText.setInputType(InputType.TYPE_CLASS_DATETIME|InputType.TYPE_DATETIME_VARIATION_DATE);
+                    break;
+            }
+
+            linearLayout.addView(editText);
+
+        }
+
+    }
+
+
+}
