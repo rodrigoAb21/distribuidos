3b689e111b2a7a2655a58e5109bd499d717df68e
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-10-21 14:59:10 -0400

    Ultimos cambios
    casi nada

--- android/app/src/main/java/com/distribuidos/uagrm/android/activities/AsignacionActivity.java
+++ android/app/src/main/java/com/distribuidos/uagrm/android/activities/AsignacionActivity.java
@@ -1,19 +1,42 @@
 package com.distribuidos.uagrm.android.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.distribuidos.uagrm.android.R;
+import com.distribuidos.uagrm.android.helpers.TokenManager;
+import com.distribuidos.uagrm.android.network.ApiService;
+import com.distribuidos.uagrm.android.network.RetrofitBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AsignacionActivity extends AppCompatActivity {
+
+    Call<String> call_logout;
+    ApiService service;
+    TokenManager tokenManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asignacion);
+
+        tokenManager = TokenManager.getInstance(getSharedPreferences("prefs", MODE_PRIVATE));
+
+        if(tokenManager.getToken() == null){
+            startActivity(new Intent(AsignacionActivity.this, LoginActivity.class));
+            finish();
+        }
+
+        service = RetrofitBuilder.createServiceWithAuth(ApiService.class, tokenManager);
     }
 
     @Override
@@ -43,6 +66,19 @@ public class AsignacionActivity extends AppCompatActivity {
     }
 
     private void logout(){
+        call_logout = service.logout();
+        call_logout.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                tokenManager.deleteToken();
+                startActivity(new Intent(AsignacionActivity.this, LoginActivity.class));
+                finish();
+            }
 
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Log.w("Log-out FAIL", t.getMessage());
+            }
+        });
     }
 }
